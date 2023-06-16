e6dca678ea42316af5f4524bcdb65a7274863cff
zakaria <za.khiraoui@gmail.com>
2023-06-16 16:29:16 -0700

    zakaria devops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,28 +7,27 @@
  */
 int main(void)
 {
-    int num1, num2;
+	int num1, num2;
 
-    for (num1 = 0; num1 <= 98; num1++)
-    {
-        for (num2 = num1 + 1; num2 <= 99; num2++)
-        {
-            putchar((num1 / 10) + '0');
-            putchar((num1 % 10) + '0');
-            putchar(' ');
-            putchar((num2 / 10) + '0');
-            putchar((num2 % 10) + '0');
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-            if (num1 != 98 || num2 != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return 0;
 }
-
