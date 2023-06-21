5c49e339edce0e4ebda9665705a54b0ac0979fb7
zakaria <za.khiraoui@gmail.com>
2023-06-20 17:53:14 -0700

    zakaria devops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void print_fibonacci(int n);
+
+int main(void)
+{
+    print_fibonacci(98);
+    return (0);
+}
+
+void print_fibonacci(int n)
+{
+    int i, carry, temp;
+    int num1[5000] = {0}, num2[5000] = {0}, result[5000] = {0};
+
+    num2[0] = 1; 
+
+    for (i = 2; i <= n; i++) 
+    {
+        carry = 0;
+        for (int j = 0; j < 5000; j++)
+        {
+            temp = num1[j] + num2[j] + carry;
+            result[j] = temp % 10;
+            carry = temp / 10;
+        }
+        for (int j = 0; j < 5000; j++)
+        {
+            num1[j] = num2[j];
+            num2[j] = result[j];
+        }
+
+        printf("%d", num2[4999]);
+        for (int j = 4998; j >= 0; j--)
+        {
+            if (num2[4999] != 0 || j < 4999)
+            {
+                printf("%d", num2[j]);
+            }
+        }
+        printf(", ");
+    }
+    printf("\n");
+}
