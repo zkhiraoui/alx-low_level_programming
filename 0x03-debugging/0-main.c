27f598b8b42675ec5f97dda1a1f3a29e5874fb41
zakaria <za.khiraoui@gmail.com>
2023-06-20 18:00:08 -0700

    zakaria devops

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "main.h"
+#include "main.h" /* Include file at the top */
 
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
-*/
+*/ /* Comments properly formatted */
 
-int main(void)
+int main(void) /* Opening brace in the same line as function declaration */
 {
-    int i;
+    int i; /* Variable declared at the beginning of the function */
 
     i = 0;  /* Changed this line from i = 98; to i = 0; */
-    positive_or_negative(i);
+    positive_or_negative(i); /* No trailing white spaces */
 
-    return (0);
-}
+    return (0); /* Proper usage of spaces around operators */
+} /* Closing brace in a line by itself */
