bddd8b6f2a395831e9cc9b37e842b0eab6978e9a
zakaria <za.khiraoui@gmail.com>
2023-06-20 17:31:19 -0700

    zakaria devops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
-/**
- * main - function to print
- *
- * Return: 0
- */
-int main(void)
-{
-	char _putchar[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(_putchar); c++)
-	{
-		_putchar(_putchar[c]);
-	}
-_putchar('\n');
-return (0);
+int main(void) {
+    printf("_putchar\n");
+    return 0;
 }
