3c5e5504c45d8e1d3765d5926f3c3d3dd57f6393
zakaria <za.khiraoui@gmail.com>
2023-06-20 17:41:09 -0700

    zakaria devops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,28 @@
  */
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
 
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product >= 10)
-                    _putchar(product / 10 + '0');
-                else
-                    _putchar(' ');
-            }
+				if (product >= 10)
+					_putchar(product / 10 + '0');
+				else
+					_putchar(' ');
+			}
 
-            _putchar(product % 10 + '0');
-        }
+			_putchar(product % 10 + '0');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
