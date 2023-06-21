1670204468954cc2988ee890a029518fbcbe724d
zakaria <za.khiraoui@gmail.com>
2023-06-20 17:29:36 -0700

    zakaria devops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char _putchar[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	unsigned int c;
 
-	for (c = 0; c < sizeof(holberton); c++)
+	for (c = 0; c < sizeof(_putchar); c++)
 	{
-		_putchar(holberton[c]);
+		_putchar(_putchar[c]);
 	}
 _putchar('\n');
 return (0);
