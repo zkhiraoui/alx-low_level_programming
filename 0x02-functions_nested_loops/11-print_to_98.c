2720f9c6926b3191b9a1593ebc6a24bbf598fe8d
zakaria <za.khiraoui@gmail.com>
2023-06-20 17:45:45 -0700

    zakaria devops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,20 +12,19 @@
  */
 void print_to_98(int n)
 {
-    if (n > 98)
-    {
-        for (; n > 98; n--)
-        {
-            printf("%d, ", n);
-        }
-    }
-    else if (n < 98)
-    {
-        for (; n < 98; n++)
-        {
-            printf("%d, ", n);
-        }
-    }
-    
-    printf("98\n");
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
 }
