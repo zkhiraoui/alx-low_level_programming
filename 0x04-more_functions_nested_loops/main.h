43829a76acca436a5a6c9939e30f9c4ef0720afe
zakaria <za.khiraoui@gmail.com>
2023-06-22 04:13:13 -0700

    zakaria devops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,6 @@ int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
