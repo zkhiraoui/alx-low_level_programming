8e5b33c3100b96103230ef44b4d52563f6995434
zakaria <za.khiraoui@gmail.com>
2023-06-22 03:50:32 -0700

    zakaria devops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
- * mul - function that prints the numbers, from 0 to 9, followed by a new line.
- * @a: the int for the paramaters of my function
- * @b: the int for the paramaters of my function
- * Return: 0
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the result of the multiplication
  */
 int mul(int a, int b)
 {
-	return (a * b);
+    return a * b;
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 int _isdigit(int c);
 int _isupper(int c);
+int mul(int a, int b);
 
 /* Other function prototypes... */
 
