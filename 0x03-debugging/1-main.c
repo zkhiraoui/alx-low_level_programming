24ae1ec208b36a126ca461537aaff6d756d2fcc6
zakaria <za.khiraoui@gmail.com>
2023-06-23 02:39:49 -0700

    zakaria devops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -7,17 +8,16 @@
 int main(void)
 {
         int i;
-
         printf("Infinite loop incoming :(\n");
-
         i = 0;
-
-        /*while (i < 10)*/
-        /*{*/
-        /*putchar(i);*/
-        /*}*/
+        
+        /**
+        * while (i < 10)
+        * {
+        * putchar(i);
+        * }
+        */
 
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }
