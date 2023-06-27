d6f9eeae94424e1f1baa08a8b5c5a04c78fff69e
zakaria <za.khiraoui@gmail.com>
2023-06-27 07:38:51 -0700

    zakaria devops

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,26 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, n = 0;
-    
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	int len = 0, n = 0;
+	
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    if (len % 2 == 0)
-    {
-        n = len / 2;
-    }
-    else
-    {
-        n = (len + 1) / 2;
-    }
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
 
-    while (str[n] != '\0')
-    {
-        _putchar(str[n]);
-        n++;
-    }
-    _putchar('\n');
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
 }
