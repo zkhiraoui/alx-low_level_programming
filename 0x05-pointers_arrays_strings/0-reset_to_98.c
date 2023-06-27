82a04783ac81f7c04e5b0223283b6fa6ccbd358e
zakaria <za.khiraoui@gmail.com>
2023-06-27 07:06:56 -0700

    zakaria devops

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Updates the value it points to to 98.
- * @n: a pointer to an int.
- * 
- * Return: Nothing.
- */
+* reset_to_98 - Updates the value it points to to 98.
+* @n: a pointer to an int.
+* 
+* Return: Nothing.
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
