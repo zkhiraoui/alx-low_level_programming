8c49407de8df4eaf3da4c499c2f880fdd4d4acf9
zakaria <za.khiraoui@gmail.com>
2023-06-27 17:44:15 -0700

    zakaria devops

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 8  // you can change the length as needed
+#define PASSWORD_LENGTH 8  /* you can change the length as needed */
 
 int main(void)
 {
-    srand(time(NULL));  // initialize the random number generator
+    int i;
+    char password[PASSWORD_LENGTH + 1];  /* +1 for the null byte */
 
-    char password[PASSWORD_LENGTH + 1];  // +1 for the null byte
+    srand(time(NULL));  /* initialize the random number generator */
 
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    for (i = 0; i < PASSWORD_LENGTH; i++)
     {
-        // Generate random character in the printable ASCII range
+        /* Generate random character in the printable ASCII range */
         password[i] = rand() % (126 - 33 + 1) + 33;
     }
 
-    password[PASSWORD_LENGTH] = '\0';  // null-terminate the string
+    password[PASSWORD_LENGTH] = '\0';  /* null-terminate the string */
 
     printf("%s\n", password);
 
