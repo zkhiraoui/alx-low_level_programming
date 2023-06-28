212a969fc5fd54219cd23b796f003b6fccc71ae6
zakaria <za.khiraoui@gmail.com>
2023-06-27 17:40:25 -0700

    zakaria devops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,27 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int started = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            result *= 10;
-            result += sign * (s[i] - '0');
-            started = 1;
-        }
-        else if (started)
-            break;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += sign * (s[i] - '0');
+			started = 1;
+		}
+		else if (started)
+			break;
 
-        i++;
-    }
+		i++;
+	}
 
-    return result;
+	return result;
 }
