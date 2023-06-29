4f64900887b747548eab2f2b025b077f738a8b68
zakaria <za.khiraoui@gmail.com>
2023-06-29 07:43:55 -0700

    zakaria devops

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,7 @@
  * Return: Always 0.
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0;
 	int b = 0;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H_INCLUDED
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
