3e6a72076c05078d38c6e1e446f7b3671384e631
zakaria <za.khiraoui@gmail.com>
2023-07-03 05:53:17 -0700

    zakaria devops

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,20 @@
 #include "main.h"
-
+/**
+ * _memset - check the code for ALX.
+ * @s: parameter for the array
+ * @b: parameter for the values in the array
+ * @n: parameter for the number of times to print the new values
+ * Return: Always 0.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int a = 0;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
 
-    return s;
+	return (s);
 }
