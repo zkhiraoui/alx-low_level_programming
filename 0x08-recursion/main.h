c8a53c69ac105cc9190ca8cc7c10b40151f5d097
zakaria <za.khiraoui@gmail.com>
2023-07-05 07:36:39 -0700

    zakaria devops

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef ALX_FILE
+#define ALX__FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int helper(int n, int i);
+int is_prime_number(int n);
+int helper_prime(int n, int i, int limit);
+int wildcmp(char *s1, char *s2)
+
+#endif
