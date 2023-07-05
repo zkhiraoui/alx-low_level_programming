0fafdadabb70ac43666e0b5dc85aca1e858303c0
zakaria <za.khiraoui@gmail.com>
2023-07-05 07:48:58 -0700

    zakaria devops

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,18 +10,23 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	/* Base case, when both strings reach to the end */
+	/*
+	 * Base case, when both strings reach to the end
+	 */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	/* If the current character of s2 is '*', then there are two possibilities
+	/*
+	 * If the current character of s2 is '*', then there are two possibilities
 	 * a) We consider current character of s2
 	 * b) We ignore current character of s2
 	 */
 	if (*s2 == '*')
 		return ((*s1 != '\0' && wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1));
 
-	/* If the current character of s1 and s2 match */
+	/*
+	 * If the current character of s1 and s2 match
+	 */
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
