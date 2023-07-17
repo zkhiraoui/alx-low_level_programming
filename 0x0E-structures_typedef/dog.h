a32e914c72722eabdf839b252504de0d5a939006
zakaria <za.khiraoui@gmail.com>
2023-07-17 04:19:45 -0700

    zakaria devops

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's attributes
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: A type that describes a dog
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
