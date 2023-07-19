224da170d935f1d59c6b807897e278e67588cff2
zakaria <za.khiraoui@gmail.com>
2023-07-19 01:50:03 -0700

    zakaria devops

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function to use to print
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+        return;
+
+    f(name);
+}
