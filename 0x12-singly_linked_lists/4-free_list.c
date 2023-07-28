07bff6b79cfbc8a8ce65a5df27ae4d14d351cdd8
zakaria <za.khiraoui@gmail.com>
2023-07-27 17:18:58 -0700

    zakaria devops

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free list.c
+ */
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
