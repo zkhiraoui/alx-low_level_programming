2d824548cbc0402520944f05d81bdf9be250937c
zkhiraoui <za.khiraoui@gmail.com>
2024-02-22 15:04:32 -0800

    zkhiraoui - alx  devops

--- 0x18-dynamic_libraries/101-make_me_win.c
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -1,14 +1,17 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-void *malloc(size_t size)
-{
-/* Override malloc() to do nothing */
-(void)size;
-return NULL;
-}
-
-int rand(void)
-{
-/* Override rand() to always return the winning numbers */
-return 9;
+// Mock version of rand() that returns predictable values
+int rand(void) {
+    static int calls = 0;
+    calls++;
+    // Return the chosen numbers based on the order of calls
+    switch (calls) {
+        case 1: return 9;
+        case 2: return 8;
+        case 3: return 10;
+        case 4: return 24;
+        case 5: return 75;
+        // Assuming the bonus number is generated differently, adjust as necessary
+        default: return 9;
+    }
 }
