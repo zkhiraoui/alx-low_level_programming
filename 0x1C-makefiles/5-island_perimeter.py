7afc9585f041749c0cec055e052977647207503c
zkhiraoui <za.khiraoui@gmail.com>
2024-02-26 08:04:42 -0800

    zkhiraoui - alx  devops

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
